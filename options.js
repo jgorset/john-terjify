ee06d6e54175d370df7065698a4b25c74524b51b
Ole Henrik Skogstrøm <oleskogstrom@gmail.com>
2015-09-02 18:16:23 +0200

    Correct indentation and refactor options script

--- options.js
+++ options.js
@@ -1,64 +1,44 @@
 var defaultName = "John Terje";
 var defaultRegex = ' (he|she|han|hun|henne|ham) ';
 
-var jtRegex;
-var jtName;
-
 function loadOptions() {
 
 	chrome.storage.sync.get("jtRegex", function(data) {
 		var regexField = document.getElementById("regex");
-		if (data.jtRegex == undefined) {
-			chrome.storage.sync.set({'jtRegex':  defaultRegex}, function() {
-				regexField.value = defaultRegex;
-			});
-		} else {
-			jtRegex = data.jtRegex;
-
-		  regexField.value = jtRegex;
-		}
+		regexField.value = data.jtRegex == undefined ? defaultRegex : data.jtRegex;
 	});
 
 	chrome.storage.sync.get("jtName", function(data) {
 		var nameField = document.getElementById("name");
-		if (data.jtName == undefined) {
-			chrome.storage.sync.set({'jtName': defaultName}, function() {
-			  nameField.value = defaultName;
-			});
-		} else {
-			jtName = data.jtName;
-		  nameField.value = jtName;
-		}
+		nameField.value = data.jtName == undefined ? defaultName : data.jtName;
 	});
 
 }
 
 function saveOptions() {
 	var name = document.getElementById("name").value;
-  chrome.storage.sync.set({'jtName': name}, function() {
-    console.log('Name saved');
-  });
+	chrome.storage.sync.set({'jtName': name}, function() {
+	});
 
-  var regex = document.getElementById("regex").value;
-  chrome.storage.sync.set({'jtRegex': regex}, function() {
-    console.log('Regex saved');
-  });
+	var regex = document.getElementById("regex").value;
+	chrome.storage.sync.set({'jtRegex': regex}, function() {
+	});
 }
 
-function eraseOptions() {
+function resetOptions() {
 	chrome.storage.sync.set({'jtName': defaultName}, function() {
-  });
-  chrome.storage.sync.set({'jtRegex': defaultRegex}, function() {
-  });
+	});
+	chrome.storage.sync.set({'jtRegex': defaultRegex}, function() {
+	});
 	location.reload();
 }
 
 $( document ).ready(function() {
-  loadOptions();
-  $('#saveBtn').click(function(){
-    saveOptions();
-  });
-  $('#eraseBtn').click(function(){
-    eraseOptions();
-  });
+	loadOptions();
+	$('#saveBtn').click(function(){
+		saveOptions();
+	});
+	$('#eraseBtn').click(function(){
+		resetOptions();
+	});
 });
--- replace.js
+++ replace.js
@@ -1,23 +1,10 @@
-var defaultName = "John Terje";
-var defaultRegex = ' (he|she|han|hun|henne|ham) ';
-var jtName;
-var jtRegex;
-
 chrome.storage.sync.get(["jtRegex", "jtName"], function(data) {
 
-  if (data.jtName == undefined) {
-    jtName = defaultName;
-  } else {
-    jtName = data.jtName;
-  }
-
-  if (data.jtRegex == undefined) {
-    jtRegex = defaultRegex;
-  } else {
-    jtRegex = data.jtRegex;
-  }
+  var jtName = data.jtName == undefined ? "John Terje" : data.jtName;
+  var jtRegex = data.jtRegex == undefined ? ' (he|she|han|hun|henne|ham) ' : data.jtRegex ;
 
   var re = new RegExp(jtRegex,"ig");
+
   $("body *").replaceText(re, " " + jtName + " ");
 
 });
