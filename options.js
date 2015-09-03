3957b2d47dc28c8c690dca272a27272ea9126e0b
Johannes Gorset <jgorset@gmail.com>
2015-09-03 22:04:31 +0200

    Convert tabs to spaces

--- options.js
+++ options.js
@@ -1,34 +1,34 @@
 $( document ).ready(function() {
-	var defaultName = "John Terje";
-	var defaultRegex = ' (he|she|han|hun|henne|ham) ';
+  var defaultName = "John Terje";
+  var defaultRegex = ' (he|she|han|hun|henne|ham) ';
 
-	var regexField = document.getElementById("regex");
-	var nameField = document.getElementById("name");
+  var regexField = document.getElementById("regex");
+  var nameField = document.getElementById("name");
 
-	function loadOptions() {
-		chrome.storage.sync.get(["jtRegex", "jtName"], function(data) {
-			regexField.value = data.jtRegex == undefined ? defaultRegex : data.jtRegex;
-			nameField.value = data.jtName == undefined ? defaultName : data.jtName;
-		});
-	}
+  function loadOptions() {
+    chrome.storage.sync.get(["jtRegex", "jtName"], function(data) {
+      regexField.value = data.jtRegex == undefined ? defaultRegex : data.jtRegex;
+      nameField.value = data.jtName == undefined ? defaultName : data.jtName;
+    });
+  }
 
-	function saveOptions() {
-		console.log(nameField)
-		chrome.storage.sync.set({'jtName': nameField.value, 'jtRegex': regexField.value}, function() {
-		});
-	}
+  function saveOptions() {
+    console.log(nameField)
+    chrome.storage.sync.set({'jtName': nameField.value, 'jtRegex': regexField.value}, function() {
+    });
+  }
 
-	function resetOptions() {
-		chrome.storage.sync.set({'jtName': defaultName, 'jtRegex': defaultRegex}, function() {
-		});
-		location.reload();
-	}
+  function resetOptions() {
+    chrome.storage.sync.set({'jtName': defaultName, 'jtRegex': defaultRegex}, function() {
+    });
+    location.reload();
+  }
 
-	loadOptions();
-	$('#saveBtn').click(function(){
-		saveOptions();
-	});
-	$('#eraseBtn').click(function(){
-		resetOptions();
-	});
+  loadOptions();
+  $('#saveBtn').click(function(){
+    saveOptions();
+  });
+  $('#eraseBtn').click(function(){
+    resetOptions();
+  });
 });
