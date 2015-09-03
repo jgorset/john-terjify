becb8f5e01b9af50f3a7c4646ff05f338c54c757
Johannes Gorset <jgorset@gmail.com>
2015-09-03 22:13:00 +0200

    Normalize quotes

--- replace.js
+++ replace.js
@@ -1,10 +1,8 @@
-chrome.storage.sync.get(["jtRegex", "jtName"], function(data) {
-
-  var jtName = data.jtName == undefined ? "John Terje" : data.jtName;
+chrome.storage.sync.get(['jtRegex', 'jtName'], function(data) {
+  var jtName = data.jtName == undefined ? 'John Terje' : data.jtName;
   var jtRegex = data.jtRegex == undefined ? ' (he|she|han|hun|henne|ham) ' : data.jtRegex;
 
-  var re = new RegExp(jtRegex,"ig");
+  var re = new RegExp(jtRegex, 'ig');
 
-  $("body *").replaceText(re, " " + jtName + " ");
-
+  $('body *').replaceText(re, ' ' + jtName + ' ');
 });
